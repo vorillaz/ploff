e77417e5971d169885a8ebd4df70d07d7406261c
vorillaz <theodorevorillas@gmail.com>
2024-03-01 18:21:00 +0200

    fix: tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/cli.ts', 'src/lib.ts'],
+  entry: ['src/cli.ts'],
   format: ['esm'],
   sourcemap: true,
   minify: false,
   target: 'esnext',
   outDir: 'dist',
-  splitting: false,
   clean: true,
   dts: true,
 });
